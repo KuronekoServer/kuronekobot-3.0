ab67b33d78e41cf8056e7c59e56497d280959b31
maamokun <45969559+maamokun@users.noreply.github.com>
2022-08-25 15:14:02 +0900

    ほんやく

--- events/player/botDisconnect.js
+++ events/player/botDisconnect.js
@@ -1,3 +1,3 @@
 module.exports = async(queue, client) => {
-    return client.say.queueMessage(client, queue, "Music stopped as I had been disconnected from the voice channel.", "RED");
+    return client.say.queueMessage(client, queue, "VCから強制切断されたため、再生を停止します。", "RED");
   };
