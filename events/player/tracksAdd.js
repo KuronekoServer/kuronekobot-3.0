132cf6767b507834cc1f6ac5bf442cb61f5d0711
maamokun <45969559+maamokun@users.noreply.github.com>
2022-08-25 15:18:24 +0900

    ほんやく

--- events/player/tracksAdd.js
+++ events/player/tracksAdd.js
@@ -1,6 +1,6 @@
 module.exports = async(queue, tracks, client) => {
 
-    queue.metadata.editReply(`Tracks Enqueued ${tracks.length}\nSource: ${tracks.source}`).then(async(msg)=>{
+    queue.metadata.editReply(`曲追加 - ${tracks.length}\n再生ソース ${tracks.source}`).then(async(msg)=>{
         setTimeout(function(){
             msg.delete();
         }, 10000);
