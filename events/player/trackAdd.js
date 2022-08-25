05aa599d9fdd802675e8306410ce840107415af9
maamokun <45969559+maamokun@users.noreply.github.com>
2022-08-25 15:17:37 +0900

    ほんやく

--- events/player/trackAdd.js
+++ events/player/trackAdd.js
@@ -4,7 +4,7 @@ module.exports = async(queue, track) => {
     if (!queue.playing || queue.tracks.length <= 0) return;
 
     const embed = new MessageEmbed()
-      .setTitle(`Track queued - Position ${queue.tracks.indexOf(track) +1}`)
+      .setTitle(`曲追加 - 再生待ち${queue.tracks.indexOf(track) +1}番目`)
       .setDescription(`[${track.title}](${track.url}) ~ [${track.requestedBy.toString()}]`)
       .setColor(queue.guild.me.displayColor || "#00FFFF");
 
