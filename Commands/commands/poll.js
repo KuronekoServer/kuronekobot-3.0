139ba441ee798ce6c6d3a4bbc749d3e544353670
BURI <85824872+TanakaTakeshikun@users.noreply.github.com>
2022-09-24 13:11:33 +0900

    関数化しててどこからも呼び出してなかった

--- Commands/commands/poll.js
+++ Commands/commands/poll.js
@@ -1,28 +1,21 @@
-const Discord = require("discord.js");
-
 module.exports = {
-  name: "poll",
-  description: "投票を作る",
-  botPerms: ["EMBED_LINKS"],
-  run: async (client, message, args) => {
-    const [title, ...choices] = args
-    if (!title) return message.channel.send({content: 'タイトルを指定してください'})
-    const emojis = ['🇦', '🇧', '🇨', '🇩', '🇪', '🇫', '🇬', '🇭', '🇮', '🇯', '🇰', '🇱', '🇲', '🇳', '🇴', '🇵', '🇶', '🇷', '🇸', '🇹']
-
-
-    if (choices.length < 2 || choices.length > emojis.length)
-        return message.channel.send({content: `選択肢は最低2つ最大20個の範囲内で指定してください`})
-
-        async function sent() {
-            const poll = await message.channel.send({
-                embeds: [
-                    {
-                        title: title,
-                        description: choices.map((c, i) => `${emojis[i]} ${c}`).join('\n')
-                    }
-                ]
-            });
-            emojis.slice(0, choices.length).forEach(emoji => poll.react(emoji))
-        }
-  },
+    name: "poll",
+    description: "投票を作る",
+    botPerms: ["EMBED_LINKS"],
+    run: async (_, message, args) => {
+        const [title, ...choices] = args;
+        if (!title) return message.channel.send({ content: 'タイトルを指定してください' });
+        const emojis = ['🇦', '🇧', '🇨', '🇩', '🇪', '🇫', '🇬', '🇭', '🇮', '🇯', '🇰', '🇱', '🇲', '🇳', '🇴', '🇵', '🇶', '🇷', '🇸', '🇹'];
+        if (choices.length < 2 || choices.length > emojis.length)
+            return message.channel.send({ content: `選択肢は最低2つ最大20個の範囲内で指定してください` });
+        const poll = await message.channel.send({
+            embeds: [
+                {
+                    title: title,
+                    description: choices.map((c, i) => `${emojis[i]} ${c}`).join('\n')
+                }
+            ]
+        });
+        emojis.slice(0, choices.length).forEach(emoji => poll.react(emoji));
+    },
 };
